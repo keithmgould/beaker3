b8470c073ec1d9996d9849781dc213865f221f1c
Keith Gould <keithmgould@gmail.com>
2017-07-16 13:23:03 -0400

    adds a pi control test

---
+++ pi_control_test/picontrol.ino
@@ -0,0 +1,115 @@
+#include <SoftwareSerial.h>
+#include <SabertoothSimplified.h>
+#include "../servoMotor.cpp"
+
+// indicator light on robot
+#define INDICATOR 36
+
+// pins for the motor encoders
+#define RH_ENCODER_A 2 // interupt pin
+#define RH_ENCODER_B 38
+#define LH_ENCODER_A 3 // interupt pin
+#define LH_ENCODER_B 40
+
+// We communicate with the sabertooth
+// motor driver over serial
+#define SerialTX 18
+
+// target speed in rpm
+#define SETSPEED_RPM 120
+#define P_CONTROL 0.05
+#define I_CONTROL 0.03
+
+// 50 Hz
+#define TIMESTEP 20
+
+long timeMarker;
+
+float prevError;
+float prevU;
+
+ServoMotor motorLeft(LH_ENCODER_A,LH_ENCODER_B, -1);
+ServoMotor motorRight(RH_ENCODER_A,RH_ENCODER_B, 1);
+
+SoftwareSerial SWSerial(NOT_A_PIN, SerialTX); // RX on no pin (unused). Tx to S1.
+SabertoothSimplified sabertooth(SWSerial); // Use SWSerial as the serial port.
+
+void leftEncoderEvent() {
+  motorLeft.encoderEvent();
+}
+
+void rightEncoderEvent() {
+  motorRight.encoderEvent();
+}
+
+void updatePower(float newGain){
+  // safety first
+  if(newGain > 100){newGain = 100;}
+  if(newGain < -100){newGain = -100;}
+
+  sabertooth.motor(1, newGain);
+  sabertooth.motor(2, newGain);
+}
+
+// kill motors and blink status indicator
+void errorMode(const char* input) {
+  Serial.println(input);
+  updatePower(0);
+  pinMode(INDICATOR, OUTPUT);
+  while(true){
+    digitalWrite(INDICATOR, HIGH);
+    delay(300);
+    digitalWrite(INDICATOR, LOW);
+    delay(300);
+  }
+}
+
+void setup(){
+  pinMode(INDICATOR, OUTPUT);
+  // turn off indicator light while we setup
+  digitalWrite(INDICATOR, LOW);
+
+  // Open serial communications
+  // and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {;}
+
+  // initialize motor encoders and interrupts
+  motorLeft.init();
+  motorRight.init();
+  delay(100);
+  attachInterrupt(digitalPinToInterrupt(LH_ENCODER_A), leftEncoderEvent, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RH_ENCODER_A), rightEncoderEvent, CHANGE);
+
+  // Sabertooth motor driver commanded over Serial
+  SWSerial.begin(9600);
+  while (!SWSerial) {;}
+  updatePower(0);
+
+  prevError = 0;
+  prevU = 0;
+  timeMarker = millis();
+}
+
+void loop(){
+  if((millis() - timeMarker) < TIMESTEP){return;}
+  timeMarker = millis();
+
+  float av = motorLeft.getAngularVelocity();
+  float current_rpm = motorLeft.getRPM();
+
+  float error = SETSPEED_RPM - current_rpm;
+  float newU = prevU + P_CONTROL * error - I_CONTROL * prevError;
+
+  Serial.print(error,5);
+  Serial.print(", ");
+  Serial.print(prevError,5);
+  Serial.print(", ");
+  Serial.print(current_rpm,5);
+  Serial.print(", ");
+  Serial.println(newU);
+  updatePower(newU);
+
+  prevError = error;
+  prevU = newU;
+}
