e2e2235e61455cf0505659b7d7e78d1a73b047a0
Keith Gould <keithmgould@gmail.com>
2017-07-17 06:54:30 -0400

    shows off utils.cpp

---
+++ tests/formatting.cpp
@@ -0,0 +1,19 @@
+#include "../utils.cpp"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[80];
+  strcpy(str, "these ");
+  strcat(str, "strings ");
+  strcat(str, "are ");
+  strcat(str, "concatenated.");
+
+  printf("%s",str);
+
+  char res[20];
+  float n = 233.007;
+  ftoa(n, res, 4);
+  printf("\n\"%s\"\n", res);
+}
+
